354b4f6527841745a02daf46cacec8fa2ef61cda
inperium <ibow9416@740s-pl-111.local>
2016-09-27 11:42:28 -0600

    Created Project

---
+++ src/popups/model/Thingy.java
@@ -0,0 +1,42 @@
+package popups.model;
+
+public class Thingy
+{
+
+	private int number;
+	private double value;
+	private String words;
+	private boolean isvalid;
+	public int getNumber()
+	{
+		return number;
+	}
+	public void setNumber(int number)
+	{
+		this.number = number;
+	}
+	public double getValue()
+	{
+		return value;
+	}
+	public void setValue(double value)
+	{
+		this.value = value;
+	}
+	public String getWords()
+	{
+		return words;
+	}
+	public void setWords(String words)
+	{
+		this.words = words;
+	}
+	public boolean isIsvalid()
+	{
+		return isvalid;
+	}
+	public void setIsvalid(boolean isvalid)
+	{
+		this.isvalid = isvalid;
+	}
+}
