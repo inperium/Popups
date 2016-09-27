7ad20ed65b3571e9694b0a6889b9d61560255389
inperium <ibow9416@740s-pl-111.local>
2016-09-27 12:23:24 -0600

    Created output method.

--- src/popups/controller/popupsController.java
+++ src/popups/controller/popupsController.java
@@ -1,6 +1,11 @@
 package popups.controller;
 
+import javax.swing.JOptionPane;
+
 public class popupsController
 {
-
+	public void displayMessage(String message)
+	{
+		JOptionPane.showMessageDialog(null, message);
+	}
 }
