431c5b660e2d79cb24818ba3477ce0528867f35b
inperium <ibow9416@740s-pl-111.local>
2016-09-29 12:23:10 -0600

    Added input verification for double values.

--- src/popups/controller/popupsController.java
+++ src/popups/controller/popupsController.java
@@ -5,12 +5,12 @@ import popups.view.PopupsViewer;
 public class PopupsController
 {
 	private PopupsViewer display;
-	
+
 	public PopupsController()
 	{
 		display = new PopupsViewer();
 	}
-	
+
 	public void start()
 	{
 		int count = 0;
@@ -20,9 +20,32 @@ public class PopupsController
 			answer = display.collectResponse("Your antivirus software has encountered a virus. Please\n enter your SSN to allow it to delete the infected file.");
 					
 		}
-
-	}
 		
+		
+	}
 	
+	/**
+	 * Checks a supplied String value to see if it can be converted to a double.
+	 * @param potentialValue The supplied String.
+	 * @return Whether the conversion to a double is possible as a boolean value.
+	 */
 	
+	private boolean isDouble(String potentialValue)
+	{
+		boolean validDouble = false;
+		
+		try
+		{
+			double testValue = Double.parseDouble(potentialValue);
+			validDouble = true;
+		}
+		catch(NumberFormatException notDoubleError)
+		{
+			
+		}
+		
+		return validDouble;
+	}
+
+
 }
